b00bf273386aba237fcc7ae34fd64f4287004de6
NikuJagajaga <32407164+NikuJagajaga@users.noreply.github.com>
2020-07-24 12:33:54 +0900

    v5

--- ConnectedTexture.js
+++ ConnectedTexture.js
@@ -1,10 +1,24 @@
 LIBRARY({
     name: "ConnectedTexture",
-    version: 4,
+    version: 5,
     shared: false,
     api: "CoreEngine"
 });
 //by NikuJagajaga (https://vk.com/pockettech)
+
+
+var BLOCK = function(coords, group, mode){
+    return ICRender.BLOCK(coords.x || 0, coords.y || 0, coords.z || 0, group, mode || false);
+};
+
+var addSurface = function(mesh, coords, dir, u, v){
+    mesh.addVertex(coords.x, coords.y, coords.z, u, v);
+    mesh.addVertex(coords.x + (dir[0] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" ? 0.5 : 0), u + 0.5, v);
+    mesh.addVertex(coords.x + (dir[1] === "x" ? 0.5 : 0), coords.y + (dir[1] === "y" ? 0.5 : 0), coords.z + (dir[1] === "z" ? 0.5 : 0), u, v + 0.5);
+    mesh.addVertex(coords.x + (dir[0] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" ? 0.5 : 0), u + 0.5, v);
+    mesh.addVertex(coords.x + (dir[1] === "x" ? 0.5 : 0), coords.y + (dir[1] === "y" ? 0.5 : 0), coords.z + (dir[1] === "z" ? 0.5 : 0), u, v + 0.5);
+    mesh.addVertex(coords.x + (dir[0] === "x" || dir[1] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" || dir[1] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" || dir[1] === "z" ? 0.5 : 0), u + 0.5, v + 0.5);
+};
 
 
 var ConnectedTexture = {
@@ -13,19 +27,6 @@ var ConnectedTexture = {
         y: ["x", "z"],
         z: ["x", "y"],
         x: ["z", "y"],
-    },
-
-    BLOCK: function(coords, group, mode){
-        return ICRender.BLOCK(coords.x || 0, coords.y || 0, coords.z || 0, group, mode || false);
-    },
-
-    addSurface: function(mesh, coords, dir, u, v){
-        mesh.addVertex(coords.x, coords.y, coords.z, u, v);
-        mesh.addVertex(coords.x + (dir[0] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" ? 0.5 : 0), u + 0.5, v);
-        mesh.addVertex(coords.x + (dir[1] === "x" ? 0.5 : 0), coords.y + (dir[1] === "y" ? 0.5 : 0), coords.z + (dir[1] === "z" ? 0.5 : 0), u, v + 0.5);
-        mesh.addVertex(coords.x + (dir[0] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" ? 0.5 : 0), u + 0.5, v);
-        mesh.addVertex(coords.x + (dir[1] === "x" ? 0.5 : 0), coords.y + (dir[1] === "y" ? 0.5 : 0), coords.z + (dir[1] === "z" ? 0.5 : 0), u, v + 0.5);
-        mesh.addVertex(coords.x + (dir[0] === "x" || dir[1] === "x" ? 0.5 : 0), coords.y + (dir[0] === "y" || dir[1] === "y" ? 0.5 : 0), coords.z + (dir[0] === "z" || dir[1] === "z" ? 0.5 : 0), u + 0.5, v + 0.5);
     },
 
     setModel: function(id, data, texture, groupName){
@@ -44,9 +45,9 @@ var ConnectedTexture = {
                 coords3 = {};
                 coords1[this.coords[axis][0]] = i & 1 ? 1 : -1;
                 coords2[this.coords[axis][1]] = i >> 1 ? 1 : -1;
-                H = this.BLOCK(coords1, group);
-                V = this.BLOCK(coords2, group);
-                D = this.BLOCK({x: coords1.x || coords2.x, y: coords1.y || coords2.y, z: coords1.z || coords2.z}, group);
+                H = BLOCK(coords1, group);
+                V = BLOCK(coords2, group);
+                D = BLOCK({x: coords1.x || coords2.x, y: coords1.y || coords2.y, z: coords1.z || coords2.z}, group);
                 u = i & 1 ? 0.5 : 0;
                 v = i >> 1 ? 0.5 : 0;
                 coords3[this.coords[axis][0]] = u;
@@ -55,9 +56,9 @@ var ConnectedTexture = {
                     mesh = new RenderMesh();
                     mesh.setBlockTexture(texture, j);
                     coords3[axis] = 0;
-                    this.addSurface(mesh, coords3, this.coords[axis], u, v);
+                    addSurface(mesh, coords3, this.coords[axis], u, v);
                     coords3[axis] = 1;
-                    this.addSurface(mesh, coords3, this.coords[axis], u, v);
+                    addSurface(mesh, coords3, this.coords[axis], u, v);
                     model = new BlockRenderer.Model(mesh);
                     switch(j){
                         case 0: render.addEntry(model).setCondition(ICRender.AND(ICRender.NOT(H), ICRender.NOT(V))); break;
@@ -91,9 +92,9 @@ var ConnectedTexture = {
                 coords3 = {};
                 coords1[this.coords[axis][0]] = i & 1 ? 1 : -1;
                 coords2[this.coords[axis][1]] = i >> 1 ? 1 : -1;
-                H = this.BLOCK(coords1, group);
-                V = this.BLOCK(coords2, group);
-                D = this.BLOCK({x: coords1.x || coords2.x, y: coords1.y || coords2.y, z: coords1.z || coords2.z}, group);
+                H = BLOCK(coords1, group);
+                V = BLOCK(coords2, group);
+                D = BLOCK({x: coords1.x || coords2.x, y: coords1.y || coords2.y, z: coords1.z || coords2.z}, group);
                 u = i & 1 ? 0.5 : 0;
                 v = i >> 1 ? 0.5 : 0;
                 coords3[this.coords[axis][0]] = u;
@@ -102,9 +103,9 @@ var ConnectedTexture = {
                     mesh = new RenderMesh();
                     mesh.setBlockTexture(texture, j);
                     coords3[axis] = 0;
-                    this.addSurface(mesh, coords3, this.coords[axis], u, v);
+                    addSurface(mesh, coords3, this.coords[axis], u, v);
                     model = new BlockRenderer.Model(mesh);
-                    T = ICRender.NOT(this.BLOCK({x: axis === "x" ? -1 : 0, y: axis === "y" ? -1 : 0, z: axis === "z" ? -1 : 0}, group));
+                    T = ICRender.NOT(BLOCK({x: axis === "x" ? -1 : 0, y: axis === "y" ? -1 : 0, z: axis === "z" ? -1 : 0}, group));
                     switch(j){
                         case 0: render.addEntry(model).setCondition(ICRender.AND(T, ICRender.NOT(H), ICRender.NOT(V))); break;
                         case 1: render.addEntry(model).setCondition(ICRender.AND(T, H, ICRender.NOT(V))); break;
@@ -115,9 +116,9 @@ var ConnectedTexture = {
                     mesh = new RenderMesh();
                     mesh.setBlockTexture(texture, j);
                     coords3[axis] = 1;
-                    this.addSurface(mesh, coords3, this.coords[axis], u, v);
+                    addSurface(mesh, coords3, this.coords[axis], u, v);
                     model = new BlockRenderer.Model(mesh);
-                    T = ICRender.NOT(this.BLOCK({x: axis === "x" ? 1 : 0, y: axis === "y" ? 1 : 0, z: axis === "z" ? 1 : 0}, group));
+                    T = ICRender.NOT(BLOCK({x: axis === "x" ? 1 : 0, y: axis === "y" ? 1 : 0, z: axis === "z" ? 1 : 0}, group));
                     switch(j){
                         case 0: render.addEntry(model).setCondition(ICRender.AND(T, ICRender.NOT(H), ICRender.NOT(V))); break;
                         case 1: render.addEntry(model).setCondition(ICRender.AND(T, H, ICRender.NOT(V))); break;
